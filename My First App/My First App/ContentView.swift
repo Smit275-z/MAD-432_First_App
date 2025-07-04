54a53fa6434ef22339e443f6ddca923ebbf6bf30
Smit275-z <w0849357@myscc.ca>
2025-07-03 21:56:11 -0300

    Initial Commit

---
+++ My First App/My First App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  My First App
+//
+//  Created by Jay Sanghani on 2025-07-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ My First App/My First App/My_First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_First_AppApp.swift
+//  My First App
+//
+//  Created by Jay Sanghani on 2025-07-03.
+//
+
+import SwiftUI
+
+@main
+struct My_First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
