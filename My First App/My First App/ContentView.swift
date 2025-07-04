3a013f15929201800dd7e44bd69d8ddfcd8c8060
Smit275-z <w0849357@myscc.ca>
2025-07-04 00:13:33 -0300

    final commit.

--- My First App/My First App/ContentView.swift
+++ My First App/My First App/ContentView.swift
@@ -10,27 +10,28 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        VStack {
-            // Display a system-provided globe icon
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            
-            // Display the user's name with custom styling
-            Text("Smit Patel")
-                .font(.title) // Setting the font size to title
-                .fontWeight(.bold) // Making the text bold
-                .foregroundColor(.blue) // Setting the text color to blue
-                .multilineTextAlignment(.center) // aligning the text to Center
-            
-            // Display a custom image from the asset library
-            Image("myPicture")
-                .resizable()
-                .aspectRatio(contentMode: .fit) // Maintains aspect ratio and fits the container
-//                .clipShape(Circle()) // Clips the image to a circular shape
-                .clipShape(RoundedRectangle(cornerRadius: 25)) // Alternative: Rounded rectangle shape
-                .frame(width: 250, height: 250)
-            .padding()
+        ZStack {
+            // Background color view
+            Color("myBackground")
+                .ignoresSafeArea()
+            VStack {
+                
+                // Display the user's name with custom styling
+                Text("Smit Patel")
+                    .font(.title) // Setting the font size to title
+                    .fontWeight(.bold) // Making the text bold
+                    .foregroundColor(.white) // Setting the text color to blue
+                    .multilineTextAlignment(.center) // aligning the text to Center
+                
+                // Display a custom image from the asset library
+                Image("myPicture")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit) // Maintains aspect ratio and fits the container
+                //                .clipShape(Circle()) // Clips the image to a circular shape
+                    .clipShape(RoundedRectangle(cornerRadius: 25)) // Alternative: Rounded rectangle shape
+                    .frame(width: 250, height: 250)
+                    .padding()
+            }
         }
         
     }
