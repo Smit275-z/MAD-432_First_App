50a10fa96ee89af8608f1e3e3c64bcc31b75368c
Smit275-z <w0849357@myscc.ca>
2025-07-03 22:44:44 -0300

    Completed Step 1. Added name text with styling and comments.

--- My First App/My First App/ContentView.swift
+++ My First App/My First App/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  My First App
 //
-//  Created by Jay Sanghani on 2025-07-03.
+//  Created by Smit Patel on 2025-07-03.
 //
 
 import SwiftUI
@@ -10,10 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            // Display a system-provided globe icon
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            
+            // Display the user's name with custom styling
+            Text("Smit Patel")
+                .font(.title) // Setting the font size to title
+                .fontWeight(.bold) // Making the text bold
+                .foregroundColor(.blue) // Setting the text color to blue
+                .multilineTextAlignment(.center) // aligning the text to Center
         }
         .padding()
     }
